953421cf3be99d2b19f0ae019f0c9c164ef42f3a
parkjinsu <koreasoo4382@naver.com>
2024-04-11 09:57:57 +0900

    7주차 수업

---
+++ 04.11/함수.py
@@ -0,0 +1,37 @@
+def main():
+    # my_list=[1,2,3,4,5,6,7,8,0]
+    # print(my_list)
+    # print(my_list[0])
+    # list_len=len(my_list)
+    # print(my_list[list_len-1])
+    # print(my_list[1:3])
+    # # count=0
+    # # while(count<3):
+        
+    # #     if(count==0):
+    # #         count+=1
+    # #         continue
+    # #     print(count)
+    # #     count+=1
+    # # print("dump.")    
+    
+    
+    
+    print("Hello World!")
+    
+    
+    
+    # for i in range(3):
+    #     print('Hello, world')
+    
+    
+    def hello():
+        for i in range(3):
+            print('Hello, world')
+    hello()
+
+
+
+    
+if __name__ == '__main__':
+    main()
